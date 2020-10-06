9f595126af03136ecb4f5b8812285ffbcc4891a0
Ekene N <ekenayy@gmail.com>
2020-10-06 18:48:07 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,17 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
+ epic_tragedy = {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :heroine_friends => []
-      }
-  }
-
+   }
+}
   
 end
